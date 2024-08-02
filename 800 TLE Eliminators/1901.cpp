1e592bceb68dc790e8e48365b5cf26c5896d8f54
Nitin Semwal <your.email@example.com>
2024-08-02 17:44:27 +0530

    1901A - Line Trip

---
+++ 800 TLE Eliminators/1901.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, x, num, gas, maxGas = 0;;
+        cin >> n >> x;
+
+        vector<int> g(n);
+        for(int i = 0; i < n; i++){
+            cin >> num;
+            g[i] = num;;
+        }
+        
+        for(int i = 0; i <= n; i++){
+            if(i == 0) gas = g[i];
+            else if(i == n) gas = 2 * (x - g[n - 1]);
+            else gas = g[i] - g[i - 1];
+            maxGas = max(gas, maxGas);
+        }
+
+        cout << maxGas << endl;
+    }
+    return 0; 
+}
